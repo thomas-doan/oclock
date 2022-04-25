24038f005822f027a6bb93286e8d670b40dae020
doan <thomas.doan@laplateforme.io>
2022-04-25 11:18:13 +0200

    start front miniteur

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timer</title>
+    <script type="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <head>
+
+    </head>
+    <main>
+
+        <div class="miniteur">
+            <div class="objet">test</div>
+            <div class="augmenter_diminuer">
+                <div>+</div>
+                <div>-</div>
+            </div>
+
+        </div>
+    </main>
+</body>
+
+</html>
